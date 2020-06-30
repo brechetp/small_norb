aa45f422f773fec538e3520c7a97560bebaab545
Pierre Brechet <brechet@mis.mpg.de>
2020-06-30 19:43:17 +0200

    small update (still bulk load)

--- smallnorb/pytorch_dataset.py
+++ smallnorb/pytorch_dataset.py
@@ -2,7 +2,7 @@ from torch.utils.data import Dataset
 import numpy as np
 import os
 import struct
-from dataset import SmallNORBDataset as SNDataset
+from .dataset import SmallNORBDataset as SNDataset
 import torch
 from PIL import Image
 import argparse
@@ -31,7 +31,10 @@ class SmallNORB(Dataset):
             self.file_cat =  os.path.join(root_dir, 'smallnorb-5x01235x9x18x6x2x96x96-testing-cat.mat')
             self.file_info =  os.path.join(root_dir, 'smallnorb-5x01235x9x18x6x2x96x96-testing-info.mat')
 
-        #self._read_header_dat
+        #self._fp_dat = open(self.file_dat, 'rb')
+        #self._read_header_dat()
+        #self._read_data_cat()
+        #self._read_data_info()
         #with open(self.file_dat, mode='rb') as f:
         #    self.header_dat = SNDataset._parse_small_NORB_header(f)
         #self.header_cat =
@@ -40,7 +43,16 @@ class SmallNORB(Dataset):
 
 
     def __len__(self):
-        return len(self.dat_data) // 2
+        return len(self.data_cat) *2
+
+
+    def _read_header_dat(self):
+        '''Read the header of the data file and store the information as a dict'''
+
+        self.header_dat = SNDataset._parse_small_NORB_header(self._fp_dat)
+        return
+
+
 
     def _fill_data_structures(self):
         """
@@ -59,42 +71,50 @@ class SmallNORB(Dataset):
         None
 
         """
-        self.dat_data  = SNDataset._parse_NORB_dat_file(self.file_dat)
-        self.cat_data  = SNDataset._parse_NORB_cat_file(self.file_cat)
-        self.info_data = SNDataset._parse_NORB_info_file(self.file_info)
+        self.data_dat  = SNDataset._parse_NORB_dat_file(self.file_dat)
+        self.data_cat  = SNDataset._parse_NORB_cat_file(self.file_cat)
+        self.data_info = SNDataset._parse_NORB_info_file(self.file_info)
         #for i, small_norb_example in enumerate(self.data[dataset_split]):
         #    small_norb_example.lighting  = info_data[i][3]
 
-    def _read_data(self, idx):
+    def _read_data_dat(self, idx):
         '''reads the data file at a given index'''
-        raise NotImplementedError
+
+
+        header = self.header_dat  # the header is 4+4+3*4 = 20 bytes long
+        num_examples, channels, height, width = header['dimensions']
+        offset = 20 + idx * height * width
+        self._fp_dat.seek(offset)
+        image = struct.unpack('<' + height * width * 'B', self._fp_dat.read(height * width))
+        image = np.uint8(np.reshape(image, newshape=(height, width)))
+
+        return image
 
     def __getitem__(self, idx):
 
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        if not 0 <= idx < self.__len__():
-            raise IndexError
+        #if not 0 <= idx < self.__len__():
+        #    raise IndexError
 
-        image_lt  = self.dat_data[2*idx]
-        image_rt  = self.dat_data[2*idx+1]
-        category  = self.cat_data[idx]
-        instance, elevation, azimuth, lighting  = self.info_data[idx]
+        #image  = self._read_data_dat(idx)
+        image  = self.data_dat[idx]
+        category  = self.data_cat[idx//2]
+        instance, elevation, azimuth, lighting  = self.data_info[idx//2]
         # returns a PIL image
         tf = self.transform if self.transform is not None else lambda x: x
-        sample = {'image_lt': tf(Image.fromarray(image_lt, mode='L')),
-                    'image_rt': tf(Image.fromarray(image_rt, mode='L')),
-                    'category': category,
-                    'intance': instance,
-                    'elevation': elevation,
-                    'azimuth': azimuth,
-                    'lighting': lighting
-                    }
+        #sample = #{'image_lt': tf(Image.fromarray(image_lt, mode='L')),
+        #sample = { 'image': tf(Image.fromarray(image, mode='L')),
+        #            'category': category,
+        #            'intance': instance,
+        #            'elevation': elevation,
+        #            'azimuth': azimuth,
+        #            'lighting': lighting
+        #            }
+        sample = tf(Image.fromarray(image, mode='L')), category
 
         return sample
-
-
 
 
 
